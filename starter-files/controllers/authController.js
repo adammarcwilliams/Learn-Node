08321f00c8555e699e04fe899a90129456d9a37f
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2018-03-05 18:55:56 +0000

    Adds forget POST method in Auth Controller

--- starter-files/controllers/authController.js
+++ starter-files/controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const crypto = require('crypto');
 
 exports.login = passport.authenticate('local', {
   failerRedirect: '/login',
@@ -23,7 +26,18 @@ exports.isLoggedIn = (req, res, next) => {
 
 exports.forgot = async (req, res) => {
   // 1. See if user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No account with that email exists');
+    return res.redirect('/login');
+  }
   // 2. Set rest tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
   // 3. Send them an email with the token
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
   // 4. Redirect to login page
+  res.redirect('/login');
 };
